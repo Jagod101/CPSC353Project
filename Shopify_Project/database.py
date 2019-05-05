56b0675b3ddb13c75f9eb77044944d47a3bf8795
ColeGotelli <gotel100@mail.chapman.edu>
2019-05-05 10:56:42 -0700

    Adds a function for users to input their information

--- Shopify_Project/database.py
+++ Shopify_Project/database.py
@@ -1,12 +1,11 @@
 import sqlite3
 
-
+conn = sqlite3.connect('Shopify.db.sqlite')
+cur = conn.cursor()
 
 def createTable():
 
-    conn = sqlite3.connect('Shopify.db.sqlite')
-    cur = conn.cursor()
-
+    # Statement for Creating the Users Table
     createTable = """CREATE TABLE IF NOT EXISTS Users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         Email VARCHAR(50),
@@ -27,9 +26,34 @@ def createTable():
 
     cur.execute(createTable)
     conn.commit()
-    print("record created")
+    print("Table created")
+
+
+def getUserInfo():
+    email = raw_input("Enter your email: ")
+    fName = raw_input("Enter your First Name: ")
+    lName = raw_input("Enter your Last Name: ")
+    shipAddr = raw_input("Enter your Shipping Address: ")
+    shipCity = raw_input("Enter your Shipping City: ")
+    state = raw_input("Enter your Shipping State: ")
+    zip = raw_input("Enter your Zip Code: ")
+    phone = raw_input("Enter your Phone Number: ")
+    card = raw_input("Enter your Credit Card Number: ")
+    cardFName = raw_input("Enter the First Name on the Card: ")
+    cardLName = raw_input("Enter the Last Name on the Card: ")
+    cardExpire = raw_input("Enter the expiration date: ")
+    cvv = raw_input("Enter the 3 digit code on the back: ")
+
+    userInfo = (email, fName, lName, shipAddr, '', shipCity, state, zip, phone, card, cardFName, cardLName, cardExpire, cvv)
+
+    # Statement for Inserting a new user into the Users Table
+    cur.execute("INSERT INTO Users('Email', 'FirstName', 'LastName', 'ShipAddr1', 'ShipAddr2', 'ShipCity', 'State', 'Zip', 'PhoneNumber', 'CreditCard', 'CardFirstName', 'CardLastName', 'CardExpire', 'CVV')"
+              "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", userInfo)
+    conn.commit()
+    print("Record inserted")
     conn.close()
 
 
 if __name__ == '__main__':
     createTable()
+    getUserInfo()
